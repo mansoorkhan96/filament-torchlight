2d4a9a46ba48122c578e5940468bdb50dff8d9c1
mansoorkhan96 <mansoorkhan96@users.noreply.github.com>
2023-05-27 13:03:02 +0000

    Fix styling

--- src/FilamentTorchlightServiceProvider.php
+++ src/FilamentTorchlightServiceProvider.php
@@ -2,13 +2,13 @@
 
 namespace Mansoor\FilamentTorchlight;
 
-use Spatie\LaravelPackageTools\Package;
 use Filament\PluginServiceProvider;
+use Spatie\LaravelPackageTools\Package;
 
 class FilamentTorchlightServiceProvider extends PluginServiceProvider
 {
     protected array $styles = [
-        'filament-torchlight' => __DIR__ . '/../resources/dist/css/filament-torchlight.css',
+        'filament-torchlight' => __DIR__.'/../resources/dist/css/filament-torchlight.css',
     ];
 
     public function configurePackage(Package $package): void
